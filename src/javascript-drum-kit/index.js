f0fbb53684ce390ff014bb8bf1156824629d7d39
weidehai <243395655@qq.com>
2021-06-25 15:02:37 +0800

    drum kit done

--- src/javascript-drum-kit/index.js
+++ src/javascript-drum-kit/index.js
@@ -1,1 +1,55 @@
-import './assets/css/index.scss'
+import "./assets/css/index.scss";
+import "./assets/index.html";
+
+let clap = document.querySelector("[clap]");
+let hihat = document.querySelector("[hihat]");
+let kick = document.querySelector("[kick]");
+let openhat = document.querySelector("[openhat]");
+let boom = document.querySelector("[boom]");
+let ride = document.querySelector("[ride]");
+let snare = document.querySelector("[snare]");
+let tom = document.querySelector("[tom]");
+let tink = document.querySelector("[tink]");
+const sounds = {
+  clap,
+  hihat,
+  kick,
+  openhat,
+  boom,
+  ride,
+  snare,
+  tom,
+  tink,
+};
+const keyMap = {
+  a: "clap",
+  s: "hihat",
+  d: "kick",
+  f: "openhat",
+  g: "boom",
+  h: "ride",
+  j: "snare",
+  k: "tom",
+  l: "tink",
+};
+let keys = document.querySelectorAll(".key");
+keys.forEach((key) => {
+  key.addEventListener("click", function () {
+    setClickStyle(this);
+    sounds[this.getAttribute("data-sound")].play();
+  });
+});
+keys.forEach((key) => {
+  key.addEventListener("transitionend", function () {
+    this.classList.remove("click");
+  });
+});
+document.addEventListener("keydown", (e) => {
+  let key = e.key;
+  sounds[keyMap[key]].play()
+  setClickStyle(document.querySelector(`[data-sound=${keyMap[key]}]`));
+});
+
+function setClickStyle(target) {
+  target.classList.add("click");
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,44 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const path = require('path')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
-module.exports={
-    entry:'./src/javascript-drum-kit/index.js',
-    output:{
-        path:path.resolve(__dirname,'./src/javascript-drum-kit/dist'),
-        filename: 'index_bundle.js'
-    },
-    devServer:{
-        contentBase: './src/javascript-drum-kit/dist',
-        port:1234
-    },
-    module:{
-      rules:[
-        {
-          test:/\.scss$/,
-          use:['style-loader','css-loader','sass-loader']
+module.exports = {
+  entry: "./src/javascript-drum-kit/index.js",
+  output: {
+    path: path.resolve(__dirname, "./src/javascript-drum-kit/dist"),
+    filename: "index_bundle.js",
+  },
+  devServer: {
+    contentBase: "./src/javascript-drum-kit/dist",
+    port: 1234,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(jpg|png)$/,
+        loader: "url-loader",
+        options: {
+          limit: 8 * 1024,
         },
-        {
-          test:/\.(jpg|png)$/,
-          loader:'url-loader',
-          options:{
-            limit:8*1024
-          }
-        }
-      ]
-    },
-    plugins:[
-        new HtmlWebpackPlugin({
-            title:"javascript drum kit",
-            template: './src/javascript-drum-kit/assets/index.html'
-        })
+      },
+      {
+        test: /\.(html)$/,
+        loader: "html-loader",
+      },
     ],
-    resolve:{
-      alias:{
-        "@drum":path.resolve('src/javascript-drum-kit')
-      }
-    }
-
-}
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/javascript-drum-kit/assets/index.html",
+      inject: "body",
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@drum": path.resolve("src/javascript-drum-kit"),
+    },
+  },
+};
